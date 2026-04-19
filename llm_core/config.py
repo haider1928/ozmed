c6bd5c14bf3bcf2a8583aafc62deb46f5235e16e
haider1928 <153923816+haider1928@users.noreply.github.com>
2026-04-19 12:19:01 +0500

    Update config.py

--- llm_core/config.py
+++ llm_core/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 load_dotenv()  # Load environment variables from .env file
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-print("KEY LOADED:", GROQ_API_KEY)
+#print("KEY LOADED:", GROQ_API_KEY)
 OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
 
 SYSTEM_PROMPT = open("llm_core/system_prompt.txt", 'r', encoding='utf-8').read()
